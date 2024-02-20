eeb325085fa8335f2204006e865c19217579cf35
Harenabs21 <hei.harena.6@gmail.com>
2024-02-20 16:37:15 +0300

    feat: implement ConvertGrayscaleService

---
+++ src/main/java/image/process/imageprocessing/service/ConvertGrayscaleService.java
@@ -0,0 +1,22 @@
+package image.process.imageprocessing.service;
+
+import org.imgscalr.Scalr;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Service
+public class ConvertGrayscaleService {
+
+    public byte[] convertToBlackAndWhite (byte[] imageData, String format, String newName) throws IOException {
+        BufferedImage originalImage = ImageIO.read(new ByteArrayInputStream(imageData));
+        BufferedImage convertedImage = Scalr.apply(originalImage,Scalr.OP_GRAYSCALE);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(convertedImage, format, baos);
+        return baos.toByteArray();
+    }
+}
